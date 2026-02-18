bbe239ef6f909f7066086f727db7b36637389479
legosp7 <legosp7@gmail.com>
2026-02-17 23:47:37 -0700

    fixed weird deprecated warning by overriding axios to use proxyfromenv 2.0

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,16 +1,63 @@
-/* src/app/articles/[slug]/page.tsx */ 
+import { createClient } from "contentful";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import { BlogItem } from "../types";
+
+const client = createClient({
+  space: process.env.SPACE_ID!,
+  accessToken: process.env.ACCESS_TOKEN!
+});
+
+export async function generateStaticParams() {
+  const queryOptions = {
+    content_type: "blogPost",
+    select: "fields.slug",
+  };
+
+  const articles = await client.getEntries(queryOptions);
+
+  return articles.items.map((article) => ({
+    slug: article.fields.slug,
+  }));
+}
+
+const fetchBlogPost = async (slug: string): Promise<BlogItem> => {
+  const queryOptions = {
+    content_type: "blogPost",
+    "fields.slug[match]": slug,
+  };
+
+  const queryResult = await client.getEntries(queryOptions);
+
+  return queryResult.items[0] as unknown as BlogItem;
+};
+
+/* src/app/blog/[slug]/page.tsx */ 
 type BlogPageProps = {
   params: Promise<{ slug: string }>;
 };
 
 export default async function BlogPage({ params }: BlogPageProps) {
-  const { slug } = await params;
+ const { slug } = await params;
 
-  return (
-    <main className="min-h-screen p-24 flex justify-center">
-      <div className="max-w-2xl">
-        <h1>you are in {slug}</h1>
-      </div>
-    </main>
-  );
+ const article = await fetchBlogPost(slug);
+ const { title, date, content } = article.fields;
+
+ return (
+   <main className="min-h-screen p-24 flex justify-center">
+     <div className="max-w-2xl">
+       <h1 className="font-extrabold text-3xl mb-2">{title}</h1>
+       <p className="mb-6 text-slate-400 ">
+          Posted on{" "}
+          {new Date(date).toLocaleDateString("en-US", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          })}
+        </p>
+        <div className="[&>p]:mb-8 [&>h2]:font-extrabold">
+          { documentToReactComponents(content) }
+        </div>
+     </div>
+   </main>
+ );
 }
